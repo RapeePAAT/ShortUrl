e6acff020f7f32e7eae7548476f8f380b001acfe
RapeePAAT <144792043+RapeePAAT@users.noreply.github.com>
2023-09-16 13:52:57 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const shortid = require('shortid')
 const app = express();
 const db = require('./model/db')
-const port = 5000;
+const port = 5000 || process.env.port;
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
